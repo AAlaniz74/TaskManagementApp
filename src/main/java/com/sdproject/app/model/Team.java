ad5e6d0b1677d05da26701f9aa888bf0d574a906
Pichvyda Tuy <31899661+PichvydaTuy@users.noreply.github.com>
2020-10-23 16:30:18 -0500

    Add setMembers()

--- src/main/java/com/sdproject/app/model/Team.java
+++ src/main/java/com/sdproject/app/model/Team.java
@@ -3,36 +3,43 @@ package com.sdproject.app.model;
 import java.util.ArrayList;
 
 public class Team {
-
-	private ArrayList<User> users;
-
-	public Team(ArrayList<User> users) {
-		this.users = users;
-	}
-
-	public Team() {
-		this.users = new ArrayList<User>();
-	}
-
-	public ArrayList<User> getTeamMembers() {
-		return users;
-	}
-
-	public int getTeamSize() {
-		return users.size();
-	}
-
-	public void addMember(User newMember) {
-		users.add(newMember);
-	}
-
-	public void removeMember(User memberToRemove) {
-		users.remove(memberToRemove);
-	}
-
-	public void removeTeam(Team team){
-		team = null;
-	}
-
+    
+    private String teamName;
+    private ArrayList<User> users;
+    
+    public Team(String newTeamName, ArrayList<User> users) {
+        this.teamName = newTeamName;
+        this.users = users;
+    }
+    
+    public Team() {
+        this.teamName = "";
+        this.users = new ArrayList<User>();
+    }
+    
+    public String getTeamName() { return this.teamName; }
+    
+    public void setTeamName(String name) { this.teamName = name; }
+    
+    public ArrayList<User> getTeamMembers() {
+        return users;
+    }
+    
+    public int getTeamSize() {
+        return users.size();
+    }
+    
+    public void setMembers(ArrayList<User> users) { this.users = users; }
+    
+    public void addMember(User newMember) {
+        users.add(newMember);
+    }
+    
+    public void removeMember(User memberToRemove) {
+        users.remove(memberToRemove);
+    }
+    
+    public void removeTeam(Team team) { team = null; }
+    
 }
 
